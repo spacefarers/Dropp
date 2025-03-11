cc941b420087bf55959087fc4b6825513e9cd4a6
Your Name <you@example.com>
2025-03-11 21:08:44 +0000

    delete everything

--- dropp.js
+++
@@ -1,9 +0,0 @@
-let filesInDropp = [];
-
-const windowArea = document.getElementById('mainWindow');
-
-windowArea.ondragover = (e) => {
-    console.log('dragging over');
-    e.preventDefault();
-    windowArea.classList.add('dragging');
-}
